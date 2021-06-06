d91c86642a99145530c19dcd5025444985f1ee22
xXTgamerXx9844 <72494265+xXTgamerXx@users.noreply.github.com>
2021-06-06 13:14:16 +0530

    Added stop command
    Press END key to stop

--- temps-0.2.py
+++ temps-0.2.py
@@ -1,6 +1,20 @@
 import psutil
 import time
 import sys
+from pynput import keyboard
+
+break_program = False
+def on_press(key):
+    global break_program
+    if key == keyboard.Key.end:
+        time.sleep(1)
+        print (u"\u001b[0m -----Stopping Program-----")
+        print (u"\u001b[31m Highest Temperature:" "°C")
+        print(u"\u001b[32m Lowest Temperature:" "°C")
+        print(u"\u001b[33m Average Temperature:" "°C")
+        print (u"\u001b[0m --------------------------")
+        break_program = True
+        return False
 
 print(
     "Welcome to CPUtemps!\n"
@@ -23,18 +37,18 @@ print(
 )
 x=float(input(u"\u001b[0m""Time interval in seconds- "))
 
-
-while True:
-    temps = psutil.sensors_temperatures()
-    time.sleep(x)
-    for name, entries in temps.items():
-        temp=psutil.sensors_temperatures()
-        for entry in entries:
-            if entry.label == "Tctl":
-                if entry.current > 70:
-                    print(u"\u001b[31m", entry.current, "°C   ", end="\r")
-                elif entry.current > 50:
-                    print(u"\u001b[33m", entry.current, "°C   ", end="\r")
-                elif entry.current > 30:
-                    print(u"\u001b[32m", entry.current, "°C   ", end="\r")
+with keyboard.Listener(on_press=on_press) as listener:
+    while break_program == False:
+        temps = psutil.sensors_temperatures()
+        time.sleep(x)
+        for name, entries in temps.items():
+            temp=psutil.sensors_temperatures()
+            for entry in entries:
+                if entry.label == "Tctl":
+                    if entry.current > 70:
+                        print(u"\u001b[31m", entry.current, "°C   ", end="\r")
+                    elif entry.current > 50:
+                        print(u"\u001b[33m", entry.current, "°C   ", end="\r")
+                    elif entry.current > 30:
+                        print(u"\u001b[32m", entry.current, "°C   ", end="\r")
     print()
